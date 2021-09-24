049f827098d751819c829e412154728be574126e
Pedro Ivan Fernandez Gonzalez <pedroifgonzalez@gmail.com>
2021-09-24 10:40:39 -0400

    Capitalize first letter always

--- textconverter.py
+++ textconverter.py
@@ -18,7 +18,7 @@ def capitalize(text: str)-> str:
     """Capitalize text"""
     return text.capitalize()
 
-def capitalize_after_one_period(text: str, capitalize_first_letter=False)-> str:
+def capitalize_after_one_period(text: str)-> str:
     """Capitalize text after every one period
     
     This function will remove any leading and trailing whitespace except for sentences between 
@@ -28,12 +28,7 @@ def capitalize_after_one_period(text: str, capitalize_first_letter=False)-> str:
     all_sentences = []
     for three_period_sentence in three_period_separated_sentences:
         sentences = three_period_sentence.split(".")
-        first_sentence = sentences[0]
-        capitalized_sentences = [sentence.strip().capitalize() for sentence in sentences[1:]]
-        capitalized_sentences.insert(0, first_sentence)
+        capitalized_sentences = [sentence.strip().capitalize() for sentence in sentences]
         all_sentences.append(". ".join(capitalized_sentences).rstrip())
-    
-    if capitalize_first_letter:
-        all_sentences[0][0] = capitalize(all_sentences[0][0])
 
     return "...".join(all_sentences)
