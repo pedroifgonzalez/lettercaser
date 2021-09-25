baf3a8ae38071b3ce638dcd5b93521c056ed38f5
Pedro Ivan Fernandez Gonzalez <pedroifgonzalez@gmail.com>
2021-09-25 01:15:24 -0400

    Add functions
    Description:
    - functions_caller
    - concatenate_functions_calls

--- utils.py
+++ utils.py
@@ -9,6 +9,17 @@ from typing import Callable
 
 import textconverter
 
+def functions_caller(*functions):
+    for function in functions:
+        function()
+
+def concatenate_functions_calls(*functions_calls):
+    """Function with the only purpose of joining function calls
+    
+    Returns True if all return values are different from False or equivalent
+    """
+    return all(functions_calls)
+
 def convert_clipoboard_content(function: Callable):
     """Retrieves clipboard content, converts it and updates it"""
     original_clipboard_content = pyperclip.paste()
